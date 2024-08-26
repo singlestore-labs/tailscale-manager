84139011bd390bf1b249a9960504bde343d3f0e7
Benjamin Staffin <benley@gmail.com>
2024-08-26 13:35:02 -0400

    Add nixos module

--- flake.nix
+++ flake.nix
@@ -35,5 +35,65 @@
           inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
         };
         devShell = self.devShells.${system}.default;
+
+        nixosModules.default = self.nixosModules.${system}.tailscale-manager;
+        nixosModules.tailscale-manager = { config, lib, pkgs, ... }:
+          with lib;
+          let
+            cfg = config.services.tailscale-manager;
+            configFile = pkgs.writeTextFile {
+              name = "tailscale-manager.json";
+              text = generators.toJSON {} {
+                routes = cfg.routes;
+                hostRoutes = cfg.hostRoutes;
+                advertiseExitNode = cfg.advertiseExitNode;
+                extraArgs = cfg.extraArgs;
+              };
+            };
+          in {
+          options.services.tailscale-manager = {
+            enable = mkEnableOption "tailscale-manager";
+            package = mkPackageOption self.packages.${system} "tailscale-manager" {};
+            interval = mkOption {
+              type = types.int;
+              default = 300;
+              description = "Interval between runs, in seconds";
+            };
+            routes = mkOption {
+              type = types.listOf types.str;
+              default = [];
+              description = "List of CIDR prefix routes to advertise";
+            };
+            hostRoutes = mkOption {
+              type = types.listOf types.str;
+              default = [];
+              description = "List of hostnames and IP addresses to add as /32 routes";
+            };
+            advertiseExitNode = mkOption {
+              type = types.bool;
+              default = false;
+              description = "Advertise as a tailscale exit node?";
+            };
+            extraArgs = mkOption {
+              type = types.listOf types.str;
+              default = [];
+              description = "Extra arguments for `tailscale set`";
+            };
+          };
+          config = mkIf cfg.enable {
+            systemd.services.tailscale-manager = {
+              after = ["tailscaled.service"];
+              wants = ["tailscaled.service"];
+              wantedBy = ["multi-user.target"];
+              serviceConfig = {
+                ExecStart = lib.escapeShellArgs [
+                  "${cfg.package}/bin/tailscale-manager" configFile
+                  "--tailscale=${config.services.tailscale.package}/bin/tailscale"
+                  "--interval=${toString cfg.interval}"
+                ];
+              };
+            };
+          };
+        };
       });
 }
